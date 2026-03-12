03f0a3843da0202c269ecc8b6abda6329b344dae
jy4education_git <jy4education@gmail.com>
2026-03-12 11:52:54 +0100

    +

--- kardinalitaet_1_1/03_1-1.sql
+++ kardinalitaet_1_1/03_1-1.sql
@@ -1,33 +1,66 @@
--- Vorbereitungen
+\! cls
 
+# Vorbereitungen
+DROP TABLE IF EXISTS design.purchases;
+DROP TABLE IF EXISTS design.products;
+DROP TABLE IF EXISTS design.servants;
+DROP TABLE IF EXISTS design.cats;
 
+# Mastertabelle (MT): unverändert
+CREATE TABLE design.cats
+(
+  id        INT         NOT NULL AUTO_INCREMENT,
+  fur_color VARCHAR(45) NOT NULL,
+  cat_name  VARCHAR(45) NOT NULL,
+  PRIMARY KEY (id)
+) COMMENT 'Tabelle der Katzen';
 
--- Mastertabelle (MT): unverändert
-
-
--- Struktur: MT
-
+# Struktur: MT
+DESCRIBE design.cats;
 
 -- Inserts: MT 
+INSERT INTO design.cats (id, cat_name,fur_color) VALUES 
+(DEFAULT, "Grizabella", "white"),
+(DEFAULT, "Alonzo", "grey"),
+(DEFAULT, "Mausi", "striped")
+;
+
+# Inhalte: MT
+SELECT * FROM design.cats;
+
+# DT Verbindung zur MT über Fremdschlüssel
+CREATE TABLE design.servants
+(
+  id           INT         NOT NULL AUTO_INCREMENT,
+  yrs_served   INT         NOT NULL,
+  servant_name VARCHAR(45) NOT NULL,
+  cats_id      INT         NOT NULL,
+  PRIMARY KEY (id)
+) COMMENT 'Tabelle der Servants';
+
+# Fremdschlüssel: DT
+ALTER TABLE design.servants
+  ADD CONSTRAINT FK_cats_TO_servants -- sprechender Name für die FK-Constraint
+    FOREIGN KEY (cats_id)
+    REFERENCES cats (id);
+
+# Inserts: DT
+INSERT INTO design.servants (id, servant_name, yrs_served, cats_id) VALUES
+(DEFAULT, 'Wolfgang Amadeus', 5, 1), -- Diener für Grizabella
+(DEFAULT, 'Maria Callas', 2, 2),  -- Diener für Alonzo
+(DEFAULT, 'Felix Mendelssohn', 10, 3); -- Diener für Mausi
+
+# Struktur: DT
+DESCRIBE design.servants;
+
+# Inhalte: DT
+SELECT * FROM design.servants;
 
 
--- Inhalte: MT
 
 
 
--- Detailtabelle: Verbindung zur MT über Fremdschlüssel
 
 
--- Fremdschlüssel: DT
 
 
--- wichtig bei 1:1  UNIQUE im fk
-
-
--- Struktur: DT
-
-
--- Inserts: DT
-
-
--- Inhalte: DT
